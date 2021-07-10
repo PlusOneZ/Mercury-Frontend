6894e7a79127131c3fb316b453e10dc97be967c0
plusone <zhuozhengyi@icloud.com>
2021-07-11 00:10:26 +0800

    Modify Homepage and index.html

--- src/components/HomePage/CategoryView.vue
+++ src/components/HomePage/CategoryView.vue
@@ -1,13 +1,90 @@
 <template>
+  <div class="mx-5 my-7">
+
+    <el-container>
+      <el-aside width="15vw">
+        <div class="text-center text-white"><h2> 分类 </h2></div>
+        <!--        TODO: Stick this to top -->
+        <el-menu
+            :uniqueOpened="true"
+            default-active="0"
+            class="rounded-xl"
+            background-color="rgba(255,255,255,0.3)"
+            text-color="#000"
+            active-color="#fff"
+            style="width: 15vw"
+        >
+
+          <el-menu-item
+              :class="{
+                'rounded-xl': (count === 0 || count === categories.length-1),
+                'rounded-b-none': (count === 0),
+                'rounded-t-none' : (count === categories.length-1)
+              }"
+              v-for="(cat, count) in categories"
+              :key="count"
+              :index="String(count)"
+          >
+            <i class="el-icon-camera-solid"></i>{{ cat }}
+          </el-menu-item>
+
+        </el-menu>
+      </el-aside>
+
+      <el-main>
+        <div class="category-window col-span-3">
+          <div
+              class="inline-flex"
+              v-for="i in 15"
+              :key="i"
+          >
+            <CommodityThumbnail>
+            </CommodityThumbnail>
+          </div>
+        </div>
+      </el-main>
+    </el-container>
+  </div>
 
 </template>
 
 <script>
+import CommodityThumbnail from "@/components/Public/CommodityThumbnail";
+
 export default {
-  name: "CategoryView"
+  name: "CategoryView",
+  components: {CommodityThumbnail},
+  data() {
+    return {
+      categories: [
+        "书籍",
+        "教材",
+        "课程用具",
+        "电器",
+        "日用品",
+        "食物",
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
+.el-menu-item.is-active {
+  color: #5dbe8a !important;
+  background: white !important;
+}
+
+.category-window {
+  @apply rounded-xl;
+  background: rgba(255, 255, 255, 0.3);
+  backdrop-filter: blur(20px);
+}
+
+
+.el-aside {
+  position: sticky !important;
+  top: 0;
+}
 
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,39 +1,36 @@
 <template>
-  <el-container class="page">
-    <el-header class="flex">
 
-    </el-header>
-    <el-main>
-      <div class="flex">
-        <div class="slide-window pr-8">
-          <SlideWindow></SlideWindow>
-        </div>
-        <PostListThumbnail></PostListThumbnail>
+  <el-main>
+    <div class="flex">
+      <div class="slide-window pr-8">
+        <SlideWindow></SlideWindow>
       </div>
-      <div>
-        <Heated></Heated>
+      <PostListThumbnail></PostListThumbnail>
+    </div>
+    <div>
+      <Heated></Heated>
 
-        <div class="grid-cols-6 grid">
-          <CommodityThumbnail
-              v-for="i in 100"
-              :key="i"
-          ></CommodityThumbnail>
-        </div>
-      </div>
-    </el-main>
-  </el-container>
+      <!--        <div class="grid-cols-6 grid">-->
+      <!--          <CommodityThumbnail-->
+      <!--              v-for="i in 100"-->
+      <!--              :key="i"-->
+      <!--          ></CommodityThumbnail>-->
+      <!--        </div>-->
+      <CategoryView> </CategoryView>
+    </div>
+  </el-main>
 </template>
 
 <script>
 import SlideWindow from "@/components/HomePage/SlideWindow";
 import PostListThumbnail from "@/components/HomePage/PostListThumbnail";
 import Heated from "@/components/HomePage/Heated";
-import CommodityThumbnail from "@/components/Public/CommodityThumbnail";
+import CategoryView from "@/components/HomePage/CategoryView";
 
 export default {
   name: "Home",
   components: {
-    CommodityThumbnail,
+    CategoryView,
     Heated,
     PostListThumbnail,
     SlideWindow
@@ -42,16 +39,13 @@ export default {
 </script>
 
 <style scoped>
-.el-header {
-  background: #0f1423;
-}
 
 .el-main {
   background: -webkit-linear-gradient(left top, #2f90b9 2.5%, #1781b5 7.5%, #93b5cf 20%, #d8e3e7 50%, #93b5cf 70%, #1781b5 92.5%, #2f90b9 97.5%); /* Safari 5.1 - 6.0 */
   background: -o-linear-gradient(bottom right, #000, #fff); /* Opera 11.1 - 12.0 */
   background: -moz-linear-gradient(bottom right, #000, #fff); /* Firefox 3.6 - 15 */
 //background-image: repeating-linear-gradient(#2f90b9 15%, #d8e3e7 15%); /* 标准的语法 */
-  background-image: linear-gradient(to top, #2f90b9, #1781b5, #93b5cf, #93b5cf);
+  background-image: linear-gradient(#5698c3, #d1c2d3);
   height: 100%
 }
 
