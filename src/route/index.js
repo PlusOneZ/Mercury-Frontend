9c07673620bd628e188a88699918a5eaf290e388
hky011011 <1150909279@qq.com>
2021-07-12 10:07:34 +0800

    add EditInfo.vue and MyOrders.vue

--- src/route/index.js
+++ src/route/index.js
@@ -9,6 +9,8 @@ const Me = () => import("../pages/Me")
 const Chat = () => import("../pages/Chat")
 const PostDetail = () => import("../pages/PostDetail")
 const OrderDetail = () => import("../pages/OrderDetail")
+const EditInfo = () => import("../pages/EditInfo")
+const MyOrders = () => import("../pages/MyOrders")
 
 const routes = [
     {
@@ -44,7 +46,7 @@ const routes = [
     {
         path: "/editInfo",
         name: "editPersonalInfo",
-        component: Home
+        component: EditInfo
     },
     {
         path: "/chat",
@@ -60,6 +62,11 @@ const routes = [
         path: "/PostDetail",
         name: "PostDetail",
         component: PostDetail
+    },
+    {
+        path: "/MyOrders",
+        name: "MyOrders",
+        component: MyOrders
     }
 ]
 
